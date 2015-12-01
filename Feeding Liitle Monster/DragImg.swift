a2cfa63548eaf634f5e901c421372d7aec0cbefc
Atef H Ataya <atef.ataya@yahoo.com>
2015-12-01 11:29:30 -0800

    Initial Commit

---
+++ Feeding Liitle Monster/DragImg.swift
@@ -0,0 +1,9 @@
+//
+//  DragImg.swift
+//  Feeding Liitle Monster
+//
+//  Created by Atef H Ataya on 12/1/15.
+//  Copyright © 2015 Atef H Ataya. All rights reserved.
+//
+
+import Foundation
--- Feeding Liitle Monster/ViewController.swift
+++ Feeding Liitle Monster/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImage: UIImageView!
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var heartImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var imgArray = [UIImage]() //empty array
+        for var x = 1; x <= 4; x++ {
+            let img = UIImage(named: "idle\(x).png")
+            imgArray.append(img!) // force unwrap; means it will definitly have a value.
+        }
+        monsterImage.animationImages = imgArray
+        monsterImage.animationDuration = 0.8
+        monsterImage.animationRepeatCount = 0 //repeat 
+        monsterImage.startAnimating()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//        print("I just touched the screen")
+//    }
 
-
+    
 }
 
